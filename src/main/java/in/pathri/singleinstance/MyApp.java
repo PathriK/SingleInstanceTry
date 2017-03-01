6f36216039a3e2fe2dad25aba77e3146fa4c79b4
PathriK <pathrikumark@gmail.com>
2017-03-01 18:18:35 +0530

    junique implementation
    Added basic static message implementation for junique

--- src/main/java/in/pathri/singleinstance/MyApp.java
+++ src/main/java/in/pathri/singleinstance/MyApp.java
@@ -1,10 +1,24 @@
 package in.pathri.singleinstance;
 
+import java.util.Scanner;
+
 public class MyApp {
 	public static final String appId = "in.pathri.singleinstance.MyApp";
+	private Scanner input;
+	private static MyApp myApp = new MyApp("init");;
+		
 	public MyApp(String msg) {
-
+		 input = new Scanner(System.in);
 		System.out.println("Init msg::" + msg);
 	}
 	
+	public static void recieveMessage(String msg){
+		System.out.println("Recieved msg::" + msg);
+		myApp.waitForExit();
+	}
+	
+	public void waitForExit(){
+		input.nextLine();
+		input.close();
+	}
 }
--- src/main/java/in/pathri/singleinstance/UniqueHandler.java
+++ src/main/java/in/pathri/singleinstance/UniqueHandler.java
@@ -2,22 +2,33 @@ package in.pathri.singleinstance;
 
 import it.sauronsoftware.junique.AlreadyLockedException;
 import it.sauronsoftware.junique.JUnique;
+import it.sauronsoftware.junique.MessageHandler;
 
 public class UniqueHandler {
-	static String appId = MyApp.appId;
+	static String appId = MyApp.appId;	
 	public static void main(String[] args) {
 		System.out.println("Into Handler");
 		boolean alreadyRunning;
 		try {
-			JUnique.acquireLock(appId);
+			JUnique.acquireLock(appId, new MessageHandler(){
+
+				@Override
+				public String handle(String msg) {
+					MyApp.recieveMessage(msg);
+					return null;
+				}
+				
+			});
 			alreadyRunning = false;
 		} catch (AlreadyLockedException e) {
 			alreadyRunning = true;
 		}
 		if (!alreadyRunning) {
-			System.out.println("App NOT already running");			
+			System.out.println("App NOT already running");
+			MyApp.recieveMessage("not alredy running");
 		}else{
 			System.out.println("App already running");
+			JUnique.sendMessage(appId,"already running");
 		}
 	}
 }
